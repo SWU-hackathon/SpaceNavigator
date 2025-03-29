7086133fa1905ac24f7f9118a1a9047b1f890971
madmaxxor2 <madmaxxor2@gmail.com>
2025-03-29 02:13:22 +0200

    Дел

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.swuteam.spacenavigator',
-  appName: 'space-navigator',
-  webDir: 'build'
-};
-
-export default config;
